bf07749734d51d44b4a3828de6b600a52da6777e
al <usernamehw@gmail.com>
2021-04-17 14:38:41 +0300

    💄

--- src/utils.ts
+++ src/utils.ts
@@ -1,35 +1,36 @@
-import { MarkdownTableOptions } from 'markdown-table';
-import { extensionConfig } from 'src/extension';
-
+/**
+ * Replace all `\n` with `<br>`
+ */
 export function ln2br(str: string) {
 	return str.replace(/\n/g, '<br>');
 }
-
+/**
+ * Replace all `\n\n` with `<br>`
+ */
 export function mdln2br(str: string) {
 	return str.replace(/\n\n/g, '<br>');
 }
-
+/**
+ * Truncate string bigger that passed charLimit
+ */
 export function truncateString(str: string, charLimit: number): string {
 	return str.length > charLimit ? `${str.slice(0, charLimit)}…` : str;
 }
-
-export function getMarkdownTableOptions(): MarkdownTableOptions {
-	return {
-		alignDelimiters: extensionConfig.alignDelimiters,
-		padding: extensionConfig.addPadding,
-		delimiterEnd: extensionConfig.addStartEndDelimiters,
-		delimiterStart: extensionConfig.addStartEndDelimiters,
-	};
-}
-
+/**
+ * Wrap string at both sides with passed character(s).
+ */
 export function wrapIn(str: string, wrapper: string) {
 	return `${wrapper}${str}${wrapper}`;
 }
-
+/**
+ * Wrap string in backticks.
+ */
 export function wrapInBackticks(str: string) {
 	return wrapIn(str, '`');
 }
-
+/**
+ * Wrap content in `<details>`/`<summary>` tags
+ */
 export function wrapInDetailsTag(text: string, header: string) {
 	return `<details><summary>${header} list</summary>
 
@@ -37,19 +38,27 @@ ${text}
 
 </details>`;
 }
-
+/**
+ * Replace all `|` with `\|`
+ */
 export function escapeVerticalBar(str: string) {
 	return str.replace(/\|/g, '\\|');
 }
-
+/**
+ * Remove prefix from the string.
+ */
 export function removePrefix(str: string, prefix: string) {
 	return str.replace(prefix, '');
 }
-
+/**
+ * Remove last character from the string.
+ */
 export function removeLastChar(str: string) {
 	return str.slice(0, -1);
 }
-
+/**
+ * Find common prefix for an array of strings.
+ */
 export function findCommonPrefix(strings: string[]) {
 	let prefix = '';
 	for (let i = 0; i < strings[0].length; i++) {
